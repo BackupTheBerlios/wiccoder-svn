8674215bb01530b1405bb985992236d53e5d181e
wonder_mice <wonder_mice@b1028fda-012f-0410-8370-c1301273da9f>
2007-05-07 18:19:56 +0000

    working version of basic_square_iterator

--- trunk/wic/libwic/iterators.h
+++ trunk/wic/libwic/iterators.h
@@ -70,9 +70,11 @@ class basic_iterator {
 	области.
 
 	Класс-параметр шаблона \c point_t определяет тип, который будет
-	использоваться для хранения двемерных координат. Тип должен поддерживать:
+	использоваться для хранения двумерных координат. Интерфейс типа должен
+	поддерживать:
 	- тип \c size_type, обозначающий тип, используемый для координат
-	- поле x
+	- метод size_type x() возвращающий координату x
+	- метод size_type y() возвращающий координату y
 */
 template <class point_t>
 class basic_square_iterator: public basic_iterator<point_t> {
@@ -85,40 +87,115 @@ public:
 	// public methods ----------------------------------------------------------
 
 	//! \brief Конструктор
-	/*!	\param[in] top_left
-		\param[in] bottom_right
-	*/
-	basic_square_iterator(const point_t &top_left,
-						  const point_t &bottom_right):
-		top_left.get_x()
-	{
+	/*!	\param[in] start Начальная точка, с которой начнётся итерация
+		\param[in] top_left Координаты верхнего-левого угла ограничивающего
+		прямоугольника 
+		\param[in] bottom_right Координаты нижнего-правого угла
+		ограничивающего прямоугольника
 
+		Координаты указываются включительно, т.е. все координаты указывают
+		на реальные точки двумерной области.
+	*/
+	basic_square_iterator(const point_t &start,
+						  const point_t &top_left,
+						  const point_t &bottom_right):
+		_point(start),
+		_top_left(top_lef), _bottom_right(bottom_right)
+	{
+		assert(_top_left.getx()	<= _point.getx());
+		assert(_point.getx()	<= _bottom_right.getx());
+		assert(_top_left.gety()	<= _point.gety());
+		assert(_point.gety()	<= _bottom_right.gety());
 	}
 
 	//! \brief Копирующий конструктор
+	/*!	\param[in] src Исходный объект
+	*/
 	basic_square_iterator(const basic_square_iterator &src):
-		_point(src._point)
-	{}
+		_point(src._point),
+		_top_left(src._top_lef), _bottom_right(src._bottom_right)
+	{
+	}
 
 	//!	\brief Деструктор
 	~basic_square_iterator();
 
 	//! \brief Определение виртуального basic_iterator::get()
-	virtual const ival_t &get() const = 0;
-
-	//! \brief Определение виртуального basic_iterator::next()
-	virtual const ival_t &next() = 0;
-
-	//! \brief Определение виртуального basic_iterator::end()
-	virtual const bool end() const = 0;
+	virtual const point_t &get() const {
+		return _point;
+	}
 
 protected:
+	// protected methods--------------------------------------------------------
+
+	//!	\brief Сдвигает текущие координаты влево
+	/*!	\param[in] x Смещение сдвига по оси x
+		\return \c true если такой сдвиг возможен и был осуществлён, иначе
+		\c false
+	*/
+	bool _move_left(const typename point_t::size_type &x) {
+		if (_top_left.getx() <=_point.getx() - x) return false;
+
+		_point.getx() -= x;
+
+		return true;
+	}
+
+	//!	\brief Сдвигает текущие координаты вправо
+	/*!	\param[in] x Смещение сдвига по оси x
+		\return \c true если такой сдвиг возможен и был осуществлён, иначе
+		\c false
+	*/
+	bool _move_right(const typename point_t::size_type &x) {
+		if (_point.getx() + x <= _bottom_right.getx()) return false;
+
+		_point.getx() += x;
+
+		return true;
+	}
+
+	//!	\brief Сдвигает текущие координаты вверх
+	/*!	\param[in] y Смещение сдвига по оси y
+		\return \c true если такой сдвиг возможен и был осуществлён, иначе
+		\c false
+	*/
+	bool _move_up(const typename point_t::size_type &y) {
+		if (_top_left.gety() <=_point.gety() - y) return false;
+
+		_point.gety() -= y;
+
+		return true;
+	}
+
+	//!	\brief Сдвигает текущие координаты вниз
+	/*!	\param[in] y Смещение сдвига по оси y
+		\return \c true если такой сдвиг возможен и был осуществлён, иначе
+		\c false
+	*/
+	bool _move_down(const typename point_t::size_type &y) {
+		if (_point.gety() + y <= _bottom_right.gety()) return false;
+
+		_point.gety() += y;
+
+		return true;
+	}
+
 private:
 	// private data ------------------------------------------------------------
 
 	//! \brief Текущее значение итератора
 	point_t _point;
 
+	//! \brief Координаты левого-верхнего угла ограничивающего прямоугольника
+	point_t _top_left;
+
+	//! \brief Координаты нижнего-правого угла ограничивающего прямоугольника
+	point_t _bottom_right;
+
+};
+
+
+class snake_square_iterator: public basic_square_iterator<p_t> {
 };
 
 
